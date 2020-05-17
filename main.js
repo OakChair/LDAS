acab23332490cc8ed23562922eb91b7cd10ac6e7
OakChair <authenticoakchair@gmail.com>
2020-05-17 22:51:45 +0100

    moved loopify into main

--- main.js
+++ main.js
@@ -72,6 +72,57 @@ function buttonUnPress() {
 customUploader.addEventListener("mouseup", buttonUnPress);
 customUploader.addEventListener("mouseleave", buttonUnPress);
 
+function loopify(uri,cb) {
+    var context = new (window.AudioContext || window.webkitAudioContext)(),
+        request = new XMLHttpRequest();
+
+    request.responseType = "arraybuffer";
+    request.open("GET", uri, true);
+
+    // XHR failed
+    request.onerror = function() {
+        cb(new Error("Couldn't load audio from " + uri));
+    };
+
+    // XHR complete
+    request.onload = function() {
+        context.decodeAudioData(request.response,success,function(err){
+          // Audio was bad
+            cb(new Error("Couldn't decode audio from " + uri));
+        });
+    };
+
+    request.send();
+
+    function success(buffer) {
+        var source;
+        function play() {
+            // Stop if it's already playing
+            stop();
+            // Create a new source (can't replay an existing source)
+            source = context.createBufferSource();
+            source.connect(context.destination);
+            // Set the buffer
+            source.buffer = buffer;
+            source.loop = true;
+            // Play it
+            source.start(0);
+        }
+
+        function stop() {
+            // Stop and clear if it's playing
+            if (source) {
+                source.stop();
+                source = null;
+            }
+        }
+
+        cb(null,{
+            play: play,
+            stop: stop
+        });
+    }
+}
 
 for (var i = 0; i < gateImagesFN.length; ++i) {
     // Loads all of the file names into actual images for rendering
