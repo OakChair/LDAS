7ad438afb2d943f341262172892c58a37eab014d
OakChair <authenticoakchair@gmail.com>
2020-05-27 01:39:45 +0100

    fixed browser check

--- browsercheck.js
+++ browsercheck.js
@@ -1,13 +1,13 @@
 var browser = (function (agent) {
     switch (true) {
-        case agent.indexOf("edge") > -1: return "edge";
-        case agent.indexOf("edg") > -1: return "chromium based edge (dev or canary)";
-        case agent.indexOf("opr") > -1 && !!window.opr: return "opera";
-        case !!navigator.brave && await navigator.brave.isBrave(): return "brave";
-        case agent.indexOf("chrome") > -1 && !!window.chrome: return "chrome";
-        case agent.indexOf("trident") > -1: return "ie";
-        case agent.indexOf("firefox") > -1: return "firefox";
-        case agent.indexOf("safari") > -1: return "safari";
+        case (agent.indexOf("edge") > -1): return "edge";
+        case (agent.indexOf("edg") > -1): return "chromium based edge (dev or canary)";
+        case (agent.indexOf("opr") > -1 && !!window.opr): return "opera";
+        case (agent.indexOf("chrome") && !!navigator.brave): return "brave";
+        case (agent.indexOf("chrome") > -1 && !!window.chrome): return "chrome";
+        case (agent.indexOf("trident") > -1): return "ie";
+        case (agent.indexOf("firefox") > -1): return "firefox";
+        case (agent.indexOf("safari") > -1): return "safari";
         default: return "other";
     }
 })(window.navigator.userAgent.toLowerCase());
